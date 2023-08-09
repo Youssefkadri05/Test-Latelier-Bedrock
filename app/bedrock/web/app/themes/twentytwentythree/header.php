7cf2a85b2efa12318e668f73bd1bf3d485fd5d96
youssef <youssef.kadri@etudiant.univ-brest.fr>
2023-08-09 17:27:14 +0200

    feat: Finalisation de la mise en page et du style de header
    J'ai terminé la mise en œuvre de la section de headedr dans le fichier header.php.

--- app/bedrock/web/app/themes/twentytwentythree/header.php
+++ app/bedrock/web/app/themes/twentytwentythree/header.php
@@ -1,24 +1,39 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html lang="fr">
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <title><?php wp_title(); ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
     <?php wp_head(); ?>
 </head>
-<body <?php body_class(); ?>>
-    <header>
-        <div class="logo"><?php bloginfo('name'); ?></div>
-        
-        <nav class="menu">
-            <?php
-                if (has_nav_menu('menu_header')) {
-                    wp_nav_menu(array(
-                        'theme_location' => 'menu_header',
-                        'menu_class' => 'custom-menu-class',
-                    ));
-                } else {
-                    echo "Il n'y a pas de menu personnalisé.";
-                }
-            ?>
-        </nav>
-    </header>
+
+<body>
+<header class="bg-black">
+    <div class="flex justify-between items-center p-4 text-white">
+        <div class="logo ml-16">
+            <img src="<?php echo get_field('logo_image'); ?>" alt="Logo">
+        </div>
+        <nav class="menu mr-16">
+    <?php
+    if (has_nav_menu('menu_header')) {
+        wp_nav_menu(array(
+            'theme_location' => 'menu_header',
+            'menu_class' => 'flex space-x-4 text-white',
+            'link_before' => '<span class="text-white">',
+            'link_after' => '</span>',
+        ));
+    } else {
+        echo "Il n'y a pas de menu personnalisé.";
+    }
+    ?>
+</nav>
+
+    </div>
+</header>
+
+
+
+
+
+
